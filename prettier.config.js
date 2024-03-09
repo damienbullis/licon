8e8249328255bd4e690b7ab234ea5d38205b47f8
Damien <80228708+damienbullis@users.noreply.github.com>
2024-03-08 19:53:01 -0600

    add prettier and biome

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+	printWidth: 100,
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+	trailingComma: 'es5',
+	semi: true,
+	parser: 'typescript',
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolveSync } from 'bun';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: resolveSync('lib/index.ts', process.cwd()),
+			name: 'licon',
+			fileName: 'licon',
+		},
+		rollupOptions: {
+			external: ['react'],
+			output: {
+				globals: {
+					react: 'React',
+				},
+			},
+		},
+	},
+});
