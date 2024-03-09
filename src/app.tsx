6a2fb62bf97261a695b5ecf52447f415c6904e53
Damien <80228708+damienbullis@users.noreply.github.com>
2024-03-09 10:33:17 -0600

    add some comps to /src

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+/**
+ * The main wrapper for development
+ */
+export function App() {
+	return (
+		<main className="flex h-full w-full items-center justify-center bg-gradient-to-bl from-indigo-950 to-slate-800">
+			<p>Icons here</p>
+			{/* <div className="flex w-3/4 items-center justify-center"></div> */}
+		</main>
+	);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { testing } from 'licon-react';
-import { helloAnything } from '../';
 import './index.css';
-function App() {
-	const res = helloAnything('world');
-	return <div>{res}</div>;
-}
+import { App } from './app';
+
 const el = document.getElementById('app');
 if (!el) throw new Error('app element not found');
 createRoot(el).render(
 	<React.StrictMode>
-		<main>
-			<App />
-		</main>
+		<App />
 	</React.StrictMode>
 );
