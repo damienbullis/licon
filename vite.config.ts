4c1c5c0cfca659ff1bfc6ef5453d6037ed44c847
Damien <80228708+damienbullis@users.noreply.github.com>
2024-03-09 07:28:15 -0600

    more config updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	build: {
+		copyPublicDir: false,
 		lib: {
 			entry: resolve(__dirname, 'lib/main.ts'),
 			name: 'licon',
 			fileName: 'licon',
 		},
 		rollupOptions: {
-			external: ['react'],
+			external: ['react', 'react/jsx-runtime', 'react-dom'],
 			output: {
 				globals: {
 					react: 'React',
